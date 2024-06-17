1ed9ad562d89a17110f78c27511e7875e0a3f3b9
mariovelascoalonso <mario.velasco@bizaway.com>
2024-06-17 16:58:00 +0200

    Feat: MoviesListFilterComponent create HTML and pass the form

--- src/app/pages/movies/movies-list/components/movies-list-filter/movies-list-filter.component.ts
+++ src/app/pages/movies/movies-list/components/movies-list-filter/movies-list-filter.component.ts
@@ -1,11 +1,28 @@
-import { ChangeDetectionStrategy, Component, DestroyRef, OnInit, inject } from '@angular/core';
+import { NgIf } from '@angular/common';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    DestroyRef,
+    Input,
+    OnInit,
+    inject,
+} from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [],
+    imports: [NgIf, ReactiveFormsModule],
     selector: 'app-movies-list-filter',
     standalone: true,
     templateUrl: './movies-list-filter.component.html',
     styleUrls: ['./movies-list-filter.component.css'],
 })
-export class MoviesListFilterComponent {}
+export class MoviesListFilterComponent {
+    // * Inputs
+    @Input() public form:
+        | FormGroup<{
+              title: FormControl<string | null>;
+              releaseYear: FormControl<string | null>;
+          }>
+        | undefined;
+}
