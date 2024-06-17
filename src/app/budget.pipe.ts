45ae258a35c5310946ec26092b1281c9aafcc4b7
mariovelascoalonso <mario.velasco@bizaway.com>
2024-06-17 08:35:35 +0200

    Feat: Create budget Pipe

---
+++ src/app/budget.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'budget',
+    standalone: true,
+})
+export class BudgetPipe implements PipeTransform {
+    transform(value: string): string {
+        if (!value) {
+            return '';
+        }
+        // Check if the value is a range or a single value
+        const parts = value.split('-');
+        if (parts.length === 2) {
+            // It's a range
+            return `$${parts[0]}-${parts[1]} million`;
+        } else {
+            // It's a single value
+            return `$${value} million`;
+        }
+    }
+}
